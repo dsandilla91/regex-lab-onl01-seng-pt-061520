4c8465623eb7db5985a133278165143775101b47
Deborah Santillan <s.debb@yahoo.com>
2020-06-30 23:25:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
 array = []
-  do word.match(/\w+aeiou/) 
+if word.match(/\w+aeiou/) = true 
+end 
+else = false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
