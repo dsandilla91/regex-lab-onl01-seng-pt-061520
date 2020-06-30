765d1aa26a63be91c86cb282e0aa515337d1fd77
Deborah Santillan <s.debb@yahoo.com>
2020-06-30 23:26:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,10 @@
 def starts_with_a_vowel?(word)
-if word.match(/\w+aeiou/) = true 
+  if word.match(/\w+aeiou/) = true 
 end 
-else  false 
 end
+  else  false 
+end
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
 
