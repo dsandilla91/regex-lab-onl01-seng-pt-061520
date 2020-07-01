cacbc0f4d75662ca30cc363ff962e46deb06c40c
Deborah Santillan <s.debb@yahoo.com>
2020-07-01 00:02:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,8 +10,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-return array = text.split("")
-
+return_array = text.split("")
+return_array = 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
